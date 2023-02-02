b347031663905b493cbf58f8348b480a802d2fdd
Stefano Pigozzi <me@steffo.eu>
2023-02-02 04:36:55 +0100

    Allow registration of multiple custom loggers

--- unimore_bda_6/log.py
+++ unimore_bda_6/log.py
@@ -4,30 +4,34 @@ import coloredlogs
 log = logging.getLogger(__name__)
 
 
-def install_log_handler(logger: logging.Logger = None):
-    if logger is None:
-        logger = logging.getLogger("unimore_bda_6")
+def install_log_handler(loggers: list[logging.Logger] = None):
+    if loggers is None:
+        loggers = [
+            logging.getLogger("__main__"),
+            logging.getLogger("unimore_bda_6"),
+        ]
 
-    coloredlogs.install(
-        logger=logger,
-        level="DEBUG",
-        fmt="{asctime} | {name:<32} | {levelname:>8} | {message}",
-        style="{",
-        level_styles=dict(
-            debug=dict(color="white"),
-            info=dict(color="cyan"),
-            warning=dict(color="yellow"),
-            error=dict(color="red"),
-            critical=dict(color="red", bold=True),
-        ),
-        field_styles=dict(
-            asctime=dict(color='magenta'),
-            levelname=dict(color='blue', bold=True),
-            name=dict(color='blue'),
-        ),
-        isatty=True,
-    )
-    log.info("Installed custom log handler!")
+    for logger in loggers:
+        coloredlogs.install(
+            logger=logger,
+            level="DEBUG",
+            fmt="{asctime} | {name:<32} | {levelname:>8} | {message}",
+            style="{",
+            level_styles=dict(
+                debug=dict(color="white"),
+                info=dict(color="cyan"),
+                warning=dict(color="yellow"),
+                error=dict(color="red"),
+                critical=dict(color="red", bold=True),
+            ),
+            field_styles=dict(
+                asctime=dict(color='magenta'),
+                levelname=dict(color='blue', bold=True),
+                name=dict(color='blue'),
+            ),
+            isatty=True,
+        )
+        log.debug("Installed custom log handler on: %s", logger)
 
 
 __all__ = (
