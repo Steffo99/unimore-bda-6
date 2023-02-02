aa980012d796e8233528a2597162a4d1b22ab5db
Stefano Pigozzi <me@steffo.eu>
2023-02-02 04:23:11 +0100

    Add licensing note

---
+++ unimore_bda_6/vendor/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module contains modules downloaded from the Internet and adapted for the project.
+
+Edits to the respective modules are released under the same license as the modules themselves.
+
+Currently:
+
+* the adaptation of :mod:`potts` is released under the CC BY-NC-SA 3.0 license.
+"""
