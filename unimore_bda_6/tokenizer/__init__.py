48ac00b548a03346ce741f08b6f982ba2df5340e
Stefano Pigozzi <me@steffo.eu>
2023-02-10 03:19:17 +0100

    Document and fix imports in `.tokenizer`

--- unimore_bda_6/tokenizer/__init__.py
+++ unimore_bda_6/tokenizer/__init__.py
@@ -1,15 +1,9 @@
-from .base import BaseTokenizer
-from .nltk_word_tokenize import NLTKWordTokenizer
-from .potts import PottsTokenizer, PottsTokenizerWithNegation
-from .plain import PlainTokenizer
-from .lower import LowercaseTokenizer
+"""
+This module contains all implemented tokenizers.
+"""
 
-
-__all__ = (
-    "BaseTokenizer",
-    "NLTKWordTokenizer",
-    "PottsTokenizer",
-    "PottsTokenizerWithNegation",
-    "PlainTokenizer",
-    "LowercaseTokenizer",
-)
+from .base import *
+from .nltk_word_tokenize import *
+from .potts import *
+from .plain import *
+from .lower import *
