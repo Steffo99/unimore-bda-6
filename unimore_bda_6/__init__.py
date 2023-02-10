14dd8045e0714f888406f8fa22cf1827078f786d
Stefano Pigozzi <me@steffo.eu>
2023-02-10 03:27:35 +0100

    Add docstring for the root module

---
+++ unimore_bda_6/__init__.py
@@ -0,0 +1,10 @@
+"""
+Sixth project for Big Data Analytics class for my Master's Degree at Unimore.
+
+For more details, see ``README.md`` or the documentation of the submodules.
+
+.. note::
+
+    This specific module is empty.
+
+"""
