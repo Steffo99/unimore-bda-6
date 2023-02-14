58554c84e07fb922cb461a4ff7e37e8f070e92ee
Stefano Pigozzi <me@steffo.eu>
2023-02-14 17:51:24 +0100

    Do not stringify processed tokens

--- unimore_bda_6/tokenizer/nltk_word_tokenize.py
+++ unimore_bda_6/tokenizer/nltk_word_tokenize.py
@@ -1,3 +1,4 @@
+import typing as t
 import nltk
 import nltk.sentiment.util
 
@@ -9,10 +10,10 @@ class NLTKWordTokenizer(BaseTokenizer):
     Tokenizer based on `nltk.word_tokenize`.
     """
 
-    def tokenize(self, text: str) -> str:
+    def tokenize(self, text: str) -> t.Iterator[str]:
         tokens = nltk.word_tokenize(text)
         nltk.sentiment.util.mark_negation(tokens, shallow=True)
-        return " ".join(tokens)
+        return tokens
 
 
 __all__ = (
